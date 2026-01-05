cb0d58a5e1a1446ae26735b2c9e0945ac46e4ce3
Nitin Singh <nitinsingh@PRL-HVVWWLTFP1.local>
2026-01-05 16:13:37 -0500

    Report January 05, 2026

--- Shell.sh
+++ Shell.sh
@@ -5,8 +5,8 @@ source /Users/nitinsingh/miniconda3/etc/profile.d/conda.sh  # or ~/miniconda3/et
 conda activate tradingweb
 
 # Uncomment the one you need:
-#python scripts/publish_report.py nifty500_signals_20260103_162513.json
-python scripts/publish_weekly.py SwingSignal_Data_2026-01-03.json
+python scripts/publish_report.py SwingSignal_Data_2026-01-05.json
+#python scripts/publish_weekly.py SwingSignal_Data_2026-01-03.json
 
 git add .
 git commit -m "Report $(date +'%B %d, %Y')"
